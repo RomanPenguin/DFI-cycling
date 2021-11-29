8ed53c281490ab64565071a5f1e3e04eda1bb594
Tyler <tbes0001@student.monash.edu>
2021-11-29 21:26:31 +1100

    Added date created and modified retrieval.

--- emotion_detection_deepface.py
+++ emotion_detection_deepface.py
@@ -7,7 +7,12 @@ from urllib.request import urlopen
 import sys, getopt
 import os
 import cv2
-
+import csv
+from datetime import datetime
+from datetime import timedelta
+ms_delay = 1000 # Change this to increase or decrease the frame emotion sampling rate
+def creation_date(path_to_file):
+    return os.path.getmtime(path_to_file)
 
 def main(argv):
     inputFilePath = ''
@@ -33,9 +38,10 @@ def main(argv):
     vidcap = cv2.VideoCapture(inputFilePath)
     success,image = vidcap.read()
     success = True
+
     framesList=[]
     while True:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*ms_delay))    # added this line
         success,image = vidcap.read()
         if success ==False:
             break 
@@ -53,6 +59,25 @@ def main(argv):
     
     for instance in obj: #iterate through each frame and pick domininant emotions to add to a list 
         dominantEmotions.append(obj[instance]['dominant_emotion'])
+    # Tyler code addition
+    # This section pulls the creation or modified date of the video files being analysed and converts it into readable format
+    # It also adds a variable (currently 1000ms) of time to the time variable for each frame
+    creation = creation_date(r"face_video.wmv")
+    initial_timestamp = datetime.fromtimestamp(creation)#.strftime('%H:%M:%S')
+    additional_time = timedelta(seconds = ms_delay/1000) # adding 1 second with the division but keeping the variables consistent
+    list_length = len(dominantEmotions)
+    times = []
+    i = 0
+    # Creating a list of all the times that will be pushed into the csv file
+    while i < list_length:
+        times.append(initial_timestamp.strftime('%H:%M:%S'))
+        i = i + 1
+        initial_timestamp = initial_timestamp + additional_time
+    with open(outputFilePath+'_dominant_emotions.txt', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(dominantEmotions)
+        writer.writerow(times)
+    f.close()
 
     obj = {'obj': obj}
     with open(outputFilePath+'_emotions.txt', 'w') as file:
@@ -65,3 +90,5 @@ if __name__ == "__main__":
 
 
 
+# Save some time and copy and paste this into Terminal
+# python3 emotion_detection_deepface.py -i face_video.wmv -o output/face_video_frames
